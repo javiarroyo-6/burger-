7d90f56592b1e738909ed6a0e059f1957bd6e95d
Javier Arroyo <arroyoj3@uci.edu>
2018-07-20 22:01:33 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double,'FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Junior Bacon CheeseBurger',FALSE);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Hamburger',FALSE);
